4108f33f16cc8d86509460d1dffbbc35ff230468
gty-code <60562489+gty-code@users.noreply.github.com>
2021-08-29 21:02:03 +1000

    90% test

--- Assets/Scenes/PrintAndHide.cs
+++ Assets/Scenes/PrintAndHide.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
+    public Renderer rend;
+
     int i = 3;
+
     // Start is called before the first frame update
     void Start()
     {
