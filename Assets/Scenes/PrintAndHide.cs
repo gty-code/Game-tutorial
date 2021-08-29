27f3b61745a4305f84a644277ea169d51133feb3
gty-code <60562489+gty-code@users.noreply.github.com>
2021-08-29 21:26:29 +1000

    Update PrintAndHide.cs

--- Assets/Scenes/PrintAndHide.cs
+++ Assets/Scenes/PrintAndHide.cs
@@ -7,16 +7,28 @@ public class PrintAndHide : MonoBehaviour
     public Renderer rend;
 
     int i = 3;
+    int testNumber;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        testNumber = Random.Range(200, 250);
+        rend = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (gameObject.CompareTag("Red") && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (gameObject.CompareTag("Blue") && i == testNumber)
+        {
+            rend.enabled = false;
+        }
+
         Debug.Log(gameObject.name + ":" + ++i);
     }
 }
